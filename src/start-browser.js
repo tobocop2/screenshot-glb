f75d876538cc2ab5ef4c928402cb2f479383f524
Tyler Rowsell <tyler.rowsell@shopify.com>
2020-01-27 10:52:16 -0500

    Use model viewer package instead of including

--- src/start-browser.js
+++ src/start-browser.js
@@ -1,6 +1,5 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
-const path = require('path');
 
 const parseDataUrl = (dataUrl) => {
   const matches = dataUrl.match(/^data:(.+);base64,(.+)$/);
